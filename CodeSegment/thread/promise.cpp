0041bb71d6ccdf5a0bfba0a84437250faf71fdd3
CHENQI\cq <448764699@qq.com>
2019-02-01 22:29:12 +0800

    promise -- future.get 是等待线程执行完毕的意思

--- CodeSegment/thread/promise.cpp
+++ CodeSegment/thread/promise.cpp
@@ -5,6 +5,9 @@
 #include<utility>
 int func(int x)
 {
+    std::cout << "2222 " << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::cout << "3333 " << std::endl;
     return x + 2;
 }
 
@@ -13,17 +16,16 @@ int main()
     std::packaged_task<int(int)> task(func);
     std::future<int> fut = task.get_future();
 
-    std::thread(task, 2).detach();
-
-    int value = fut.get();
+    std::thread(std::move(task), 2).detach();
+    std::cout << "11111 " << std::endl;
+    int value = fut.get(); //这里会等待线程结束在往下执行
     std::cout << "the result is " << value << std::endl;
 
-    std::vector<std::future<int>> v;
+    std::vector<std::shared_future<int>> v;
     auto f = std::async(std::launch::async,
         [](int a, int b) { return a + b; }, 2, 3);
-    v.push_back(f);
-    std::cout << "the shared_future result is " << value << std::endl;
-
+    v.push_back(std::move(f));
+    std::cout << "the shared_future result is " << v[0].get() << std::endl;
 
     return 0;
 }
