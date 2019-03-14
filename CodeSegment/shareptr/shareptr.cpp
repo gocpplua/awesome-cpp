3ef963e849f7658365bab904f125b3b264ab4a95
chenqi <chenqi@ct108.com>
2019-03-14 10:56:28 +0800

    测试erase以后，对智能指针的影响，结论是：erase之后，只是对智能指针的引用计数减去1

--- CodeSegment/shareptr/shareptr.cpp
+++ CodeSegment/shareptr/shareptr.cpp
@@ -9,7 +9,9 @@ class MyClass
 {
 public:
     MyClass();
+    MyClass(int nUserID) { a = nUserID; };
     ~MyClass();
+    int a = 0;
 };
 
 MyClass::MyClass()
@@ -37,13 +39,19 @@ std::shared_ptr<MyClass> GetUser(int userid)
     return itr->second;
 }
 
+void AddUser(int userid, const std::shared_ptr<MyClass> &user)
+{
+    std::lock_guard<std::mutex> users_lock(rooms_mutex_);
+    rooms_[userid] = user;
+}
+
 int main()
 {
     std::shared_ptr<MyClass> class1 = GetUser(1);
     std::shared_ptr<MyClass> class2 = GetUser(2);
     std::unordered_map<int, std::shared_ptr<MyClass>> rooms1 = rooms_; // map是进行深拷贝的
 
-    for (int i = 0; i < 1000000000; i++)
+    for (int i = 0; i < 1; i++)
     {
         {
             std::this_thread::sleep_for(std::chrono::microseconds(1000));
@@ -52,6 +60,32 @@ int main()
         }
         int k = 0;
     }
+
+    // 测试erase以后，对智能指针的影响，结论是：erase之后，只是对智能指针的引用计数减去1
+    {
+        std::weak_ptr<MyClass> weakClass11;
+        {
+            int nUserID = 11;
+            AddUser(nUserID, std::make_shared<MyClass>(nUserID));
+            std::shared_ptr<MyClass> class11 = GetUser(nUserID);
+            weakClass11 = class11;
+            std::cout << "智能指针引用数1:" << weakClass11.use_count() << std::endl;
+            rooms_.erase(nUserID);
+            std::cout << "智能指针引用数2:" << weakClass11.use_count() << std::endl;
+            std::lock_guard<std::mutex> users_lock(rooms_mutex_);
+            class11->a = 1;
+        }
+        std::cout << "智能指针引用数3:" << weakClass11.use_count() << std::endl;
+        if (weakClass11.expired())
+        {
+            std::cout << "智能指针已经被释放" << std::endl;
+        }
+        else
+        {
+            std::cout << "智能指针引用数4:" << weakClass11.use_count() <<std::endl;
+        }
+    }
+
 
     std::cout << "Hello World!\n";
 }
