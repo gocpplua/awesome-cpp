1ef1e83c47cfacfe803f0a0cdc07b5ce9aa04527
chenqi <chenqi@ct108.com>
2019-12-11 21:09:03 +0800

    构造函数

---
+++ CodeSegment/YQTools/constructor.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    int x;
+    A(int x) : x(x)
+    {
+        cout << "Constructor" << endl;
+    }
+    A(A& a) : x(a.x)
+    {
+        cout << "Copy Constructor" << endl;
+    }
+    A& operator=(A& a)
+    {
+        x = a.x;
+        cout << "Copy Assignment operator" << endl;
+        return *this;
+    }
+    A(A&& a) : x(a.x)
+    {
+        cout << "Move Constructor" << endl;
+    }
+    A& operator=(A&& a)
+    {
+        x = a.x;
+        cout << "Move Assignment operator" << endl;
+        return *this;
+    }
+};
