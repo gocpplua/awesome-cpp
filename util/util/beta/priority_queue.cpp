efdc566d10e9eed3e7f97287beb926c407e5137d
chenqi <chenqi@ct108.com>
2021-01-21 18:39:43 +0800

    msgbus
    priority_queue

---
+++ util/util/beta/priority_queue.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+#include <queue>
+using namespace std;
+int mainxx()
+{
+	// 使用优先级队列实现最大堆最小堆：时间复杂度有O(logK)。
+	// https://blog.csdn.net/nisxiya/article/details/45725857
+	// http://www.sobugou.com/blog/2015/09/01/hashmapshi-li-:qiu-top-k/
+	unordered_map<int, int> map = { {1,1},{3,3},{2,2},{4,4},{5,5},{7,7},{6,6} };
+	struct node {
+		int userid;
+		int deposit;
+		node(int a = 0, int b = 0) :userid(a), deposit(b) {}
+	};
+
+	struct cmp {
+		bool operator()(node a, node b) {
+			return a.deposit > b.deposit;
+		}
+	};
+	priority_queue<node, vector<node>, cmp> q;
+	q.push(node(0, 0));
+	q.push(node(0, 0));
+	q.push(node(0, 0));
+	for (const auto& v : map)
+	{
+		std::cout << "q.top() is now " << q.top().userid << " " << q.top().deposit << '\n';
+		if (v.second > q.top().deposit)
+		{
+			q.pop();
+			q.push(node(v.first, v.second));
+		}
+	}
+	return 0;
+}
---
+++ util/util/msgbus.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+#include<string>
+#include <shared_mutex>
+#include <map>
+#include <any>
+#include <iostream>
+using namespace std;
+
+namespace util {
+/// 消息总线实现
+/// 该总线会在publish时同线程同步调用所有注册函数
+/// 若使用异步调用可使用buspool(一个带线程池的总线)
+///
+/// 
+
+	namespace msgbus {
+		class msgbus{ // 可以继承 private boost::noncopyable 实现禁止copy类
+		public:
+			template<typename F>
+			void subscribe(string& topic, F&& f);
+
+			template<typename F>
+			void publish(const string& topic);
+
+		private:
+			std::shared_mutex mutex_;
+			//std::multimap<string, std::any> msg_map_;
+		};
+
+
+
+
+		template<typename F>
+		void msgbus::subscribe(std::string& topic, F&& f) {
+
+		}
+		
+		template<typename F>
+		void msgbus::publish(const string& topic)
+		{
+
+		}
+
+	}
+}
