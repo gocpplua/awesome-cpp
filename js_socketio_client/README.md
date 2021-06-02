9d85ec5ebaa71449125f6313ef07871e00d1de34
琦 陈 <448764699@qq.com>
2021-06-02 18:13:47 +0800

    js socketio client

---
+++ js_socketio_client/README.md
@@ -0,0 +1,8 @@
+
+# Socket.IO Example
+
+```
+$ npm install
+$ node client_no_ssl.js   # to run the socketio client without ssl, Default port is 3000
+$ node client_ssl.js      # to run the socketio client without ssl, Default port is 3001
+```
---
+++ js_socketio_client/client_no_ssl.js
@@ -0,0 +1,55 @@
+const io = require('socket.io-client')
+const port =  3000;
+const socket = io.connect('http://127.0.0.1:' + port);
+console.log(`connect http://127.0.0.1:${port}`)
+let last;
+function send () {
+  last = new Date();
+  socket.emit('ping_from_client');
+}
+function send () {
+  last = new Date();
+  socket.emit('ping_from_client');
+}
+
+socket.on('connect_error', (err) => {
+  console.log(`connect_error ${err} `);
+});
+
+socket.on('connect_timeout', () => {
+  console.log(`connect_timeout ${socket.id}`);
+});
+
+socket.on('reconnect', () => {
+  console.log(`reconnect ${socket.id}`);
+});
+
+socket.on('reconnect_error', () => {
+  console.log(`reconnect_error ${socket.id}`);
+});
+
+socket.on('reconnect_failed', () => {
+  console.log(`reconnect_failed ${socket.id}`);
+});
+
+socket.on('connect', () => {
+  console.log(`connect ${socket.id}`);
+  send();
+});
+
+socket.on('disconnect', () => {
+  console.log(`disconnect ${socket.id}`);
+});
+
+socket.on('error', () =>{
+  console.log(`error ${socket.id}`);
+})
+socket.on('pong_from_server', () => {
+  const latency = new Date() - last;
+  console.log('latency is ' + latency + ' ms');
+  setTimeout(send, 1000);
+});
+
+socket.on('message',(data)=>{
+  console.log(data)
+})
