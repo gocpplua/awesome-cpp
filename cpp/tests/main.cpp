7275b933109a5d088056bd654ea62dff0e847bc7
chenqi <chenqi1@sensetime.com>
2021-09-06 22:14:02 +0800

    yaml map

--- cpp/tests/main.cpp
+++ cpp/tests/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
-
+#include "yaml-cpp/yaml.h"
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    YAML::Node test = YAML::LoadFile("../../test_yaml/file.yaml");
+    try
+    {
+        std::cout << test.IsNull() << std::endl;
+        std::cout << test.IsMap() << std::endl;
+        // 不能使用 test["http_server"];
+        for(YAML::const_iterator it = test.begin(); it != test.end(); it++){
+            std::cout << it->first.as<std::string>() << std::endl;
+        }
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+    
+    return 0;    
 }
+
+
+
+
