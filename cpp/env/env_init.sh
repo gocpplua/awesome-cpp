8512b138c978371b0440cde015f26597ba01ebc4
chenqi <chenqi1@sensetime.com>
2021-09-06 21:36:07 +0800

    env/env_init.sh  yaml

---
+++ cpp/env/env_init.sh
@@ -0,0 +1,6 @@
+
+# install yaml-cpp
+wget https://github.com/jbeder/yaml-cpp/archive/refs/tags/yaml-cpp-0.7.0.tar.gz -O yaml-cpp-0.7.0.tar.gz
+tar xf yaml-cpp-0.7.0.tar.gz
+cd yaml-cpp-yaml-cpp-0.7.0  && mkdir build && cd build && cmake -DYAML_BUILD_SHARED_LIBS=ON .. && make -j8 && make install
+cd ../../ && rm -rf yaml-cpp*
