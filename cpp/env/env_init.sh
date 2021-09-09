226345c7488bec8b1ae969d66ff178dbcc84ecec
chenqi <chenqi1@sensetime.com>
2021-09-09 11:10:41 +0800

    evpp install readme

--- cpp/env/env_init.sh
+++ cpp/env/env_init.sh
@@ -4,3 +4,7 @@ wget https://github.com/jbeder/yaml-cpp/archive/refs/tags/yaml-cpp-0.7.0.tar.gz 
 tar xf yaml-cpp-0.7.0.tar.gz
 cd yaml-cpp-yaml-cpp-0.7.0  && mkdir build && cd build && cmake -DYAML_BUILD_SHARED_LIBS=ON .. && make -j8 && make install
 cd ../../ && rm -rf yaml-cpp*
+
+
+# install evpp
+# 参考: https://note.youdao.com/s/BR9BvHGb
