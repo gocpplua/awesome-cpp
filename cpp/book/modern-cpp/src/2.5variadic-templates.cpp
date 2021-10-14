9e41f2cfcc68cd45750bc27a782e19662c712e83
chenqi <chenqi1@sensetime.com>
2021-10-14 09:07:46 +0800

    delete useless code

--- cpp/book/modern-cpp/src/2.5variadic-templates.cpp
+++ cpp/book/modern-cpp/src/2.5variadic-templates.cpp
@@ -58,15 +58,6 @@ auto printf3(T value, Ts... args) {
     }(), value)...};
 }
 //*/
-template<typename T, typename... Ts>
-auto printf3(T value, Ts... args){
-  std::cout << value << std::endl;
-  (void) std::initializer_list<T>{
-    ([&args]{
-
-    }, value)...
-  };
-}
 
 // 方法四：　折叠表达式　https://www.zhihu.com/question/443285720/answer/1719851060
 // 在元编程场景下初始化列表是能够保证模板可变参数包从左往右执行
