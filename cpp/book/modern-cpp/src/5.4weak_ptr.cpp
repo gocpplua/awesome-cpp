71f252cd25195da604a62a1955b69cd7f1551504
chenqi <chenqi1@sensetime.com>
2021-10-29 14:05:08 +0800

    weak_ptr

---
+++ cpp/book/modern-cpp/src/5.4weak_ptr.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <memory>
+
+struct A;
+struct B;
+
+struct A {
+    std::shared_ptr<B> pointer;
+    ~A() {
+        std::cout << "A 被销毁" << std::endl;
+    }
+};
+struct B {
+    std::shared_ptr<A> pointer;
+    ~B() {
+        std::cout << "B 被销毁" << std::endl;
+    }
+};
+int main() {
+    auto a = std::make_shared<A>();
+    auto b = std::make_shared<B>();
+    a->pointer = b;
+    b->pointer = a;
+}
