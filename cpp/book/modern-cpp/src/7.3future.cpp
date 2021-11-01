90df0186b0242a35daf0ad94f63868400638cd70
chenqi <chenqi1@sensetime.com>
2021-11-01 16:12:32 +0800

    future

---
+++ cpp/book/modern-cpp/src/7.3future.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <future>
+#include <thread>
+
+int main() {
+    // 将一个返回值为7的 lambda 表达式封装到 task 中
+    // std::packaged_task 的模板参数为要封装函数的类型
+    std::packaged_task<int()> task([](){return 7;});
+    // 获得 task 的期物
+    std::future<int> result = task.get_future(); // 在一个线程中执行 task
+    std::thread(std::move(task)).detach();
+    std::cout << "waiting...";
+    result.wait(); // 在此设置屏障，阻塞到期物的完成
+    // 输出执行结果
+    std::cout << "done!" << std:: endl << "future result is " << result.get() << std::endl;
+    return 0;
+}
