ce61dfc0bcd1077e1f85827759c3a4371c0960be
chenqi <chenqi1@sensetime.com>
2021-11-01 15:50:15 +0800

    thread

---
+++ cpp/book/modern-cpp/src/7.1thread.cpp
@@ -0,0 +1,11 @@
+// clang++ book/modern-cpp/src/7.1thread.cpp -pthread
+#include <iostream>
+#include <thread>
+
+int main() {
+    std::thread t([](){
+        std::cout << "hello world." << std::endl;
+    });
+    t.join();
+    return 0;
+}
