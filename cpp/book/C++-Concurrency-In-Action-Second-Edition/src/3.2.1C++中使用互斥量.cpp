f7c1230c6e0da5f909932c4cdede221be32d3f2a
chenqi <chenqi1@sensetime.com>
2021-11-11 15:42:34 +0800

    3.2.1C++中使用互斥量

--- cpp/book/C++-Concurrency-In-Action-Second-Edition/src/3.2.1C++中使用互斥量.cpp
+++ cpp/book/C++-Concurrency-In-Action-Second-Edition/src/3.2.1C++中使用互斥量.cpp
@@ -9,13 +9,14 @@ std::mutex mtx_;
 std::list<int> l_;
 
 void add(int i){
+  std::lock_guard<std::mutex>  lock(mtx_); // C++11  等价于下面
   //std::scoped_lock lock(mtx_); // C++17  等价于下面
-  std::lock_guard<std::mutex> lock(mtx_); // C++11
+  // std::lock_guard  lock(mtx_); // C++17中添加了一个新特性，称为模板类参数推导
   l_.emplace_back(i);
 }
 
 void findi(int i){
-  std::scoped_lock lock(mtx_);
+  std::lock_guard lock(mtx_);
   std::find(l_.begin(), l_.end(), i);
 }
 
